4eec28e4e1352f1f97ce182b5be8241345377fa4
Jared Wahlstrand <jwahlstrand@gmail.com>
2022-11-17 07:47:49 -0500

    events: mouse_position and entered_window

--- src/events.jl
+++ src/events.jl
@@ -68,13 +68,38 @@ which is not in scene coordinates, with the upper left window corner being 0
 [GLFW Docs](http://www.glfw.org/docs/latest/group__input.html#ga1e008c7a8751cea648c8f42cc91104cf)
 """
 function Makie.mouse_position(scene::Scene, screen::GLMakie.Screen{Gtk4.GtkWindowLeaf})
+    window = Makie.to_native(screen)
+    g = Gtk4.GtkEventControllerMotion(window)
+    event = scene.events.mouseposition
+    hasfocus = scene.events.hasfocus
+    function on_motion(controller, x, y)
+        if hasfocus[]
+            event[] = (x,y) # TODO: retina factor
+        end
+        nothing
+    end
+    # for now, put enter and leave in here, since they share the same event controller
+    entered = scene.events.entered_window
+    function on_enter(controller, x, y)
+        entered[] = true
+        nothing
+    end
+    function on_leave(controller)
+        entered[] = false
+        nothing
+    end
+    signal_connect(on_motion, g, "motion")
+    signal_connect(on_enter, g, "enter")
+    signal_connect(on_leave, g, "leave")
 end
 
 function Makie.scroll(scene::Scene, window::GTKGLWindow)
 end
 
 function Makie.hasfocus(scene::Scene, window::GTKGLWindow)
+    # TODO: figure out to GLFW's "hasfocus" onto GTK's behavior
 end
 
 function Makie.entered_window(scene::Scene, window::GTKGLWindow)
+    # event for this is currently in mouse_position
 end
