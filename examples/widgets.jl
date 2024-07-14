cc6e8b4e9789538a785dc173fefbbd594350faf7
Jared Wahlstrand <jwahlstrand@gmail.com>
2024-07-14 08:32:45 -0400

    get widgets.jl example working on Mac OS

--- examples/widgets.jl
+++ examples/widgets.jl
@@ -1,4 +1,13 @@
-using Gtk4Makie, Gtk4
+using Gtk4
+# Gt4kMakie seems to need the GLib main loop to be running, which takes a little
+# while on a Mac in an interactive session, due to the libuv stuff
+# FIXME
+if Sys.isapple()
+    while !istaskstarted(Gtk4.GLib.glib_main_task)
+        sleep(0.001)
+    end
+end
+using Gtk4Makie
 
 # Simple example using GtkMakieWidget
 
@@ -13,5 +22,6 @@ show(win)
 push!(p[1],lines(rand(10)))
 push!(p[2],scatter(rand(10)))
 
+# not needed, just demonstrates that this works
 empty!(p[1])
 push!(p[1],lines(rand(10)))
