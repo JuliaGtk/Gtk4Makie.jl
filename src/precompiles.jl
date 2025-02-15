f4f99f76816779706f531e7bcd33e1946a704228
Jared Wahlstrand <jwahlstrand@gmail.com>
2025-02-15 18:34:33 -0500

    skip precompiling if Gtk4 fails to initialize (like on a headless system)

--- src/precompiles.jl
+++ src/precompiles.jl
@@ -4,6 +4,10 @@ let
     @setup_workload begin
         x=rand(5)
         @compile_workload begin
+            if !Gtk4.initialized[]
+                @warn("Gtk4Makie precompile skipped: Gtk4 could not be initialized (are you on a headless system?)")
+                return
+            end
             screen = GTKScreen()
             display(screen, lines(x))
             d=DataInspector()
